4e4eeb7a9184521e0cbf615f7f02aa5b249dc27b
Shekar EliteStratus <shekar_bv@yahoo.com>
2017-09-04 21:00:44 -0400

    Cleaning the source and staging structures

--- Tiger/.settings/data/Jun 24/Build.bat
+++
@@ -1,3 +0,0 @@
-cmd /c mvn clean compile test-compile
-cmd /c mvn package -DskipTests
-copy target\Tiger-1.0.0.jar .
--- Tiger/deploy/Build.bat
+++ Tiger/deploy/Build.bat
@@ -1,3 +1,4 @@
+cd ..
 cmd /c mvn clean compile test-compile
 cmd /c mvn package -DskipTests
 copy target\Tiger-1.0.0.jar .
